399d1ea7d6dd70e7cecb33bc5a39a836c5370d95
root <root@DESKTOP-CI4O9MT.localdomain>
2021-11-21 19:21:12 +0530

    Created new function show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,15 @@
 class TodosController < ApplicationController
   def index
     #render plain: "Hello, this is /todos!"
-    render plain: Todo.all.to_a
+    #render plain: Todo.all.to_a
+    #render plain: Todo.all.map {|todo| todo.to_pleasant}.join("\n")
+    render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant}.join("\n")
+  end
+  def show
+    id = params[:id].to_i
+    render plain: "The id enter was #{id}"
+    todo= Todo.find(id)
+    render plain: todo.to_pleasant
   end
 end
 
