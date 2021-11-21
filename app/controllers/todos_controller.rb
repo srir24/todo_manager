188fe405f20496a06a7dcbaebe58e32272652677
root <root@DESKTOP-CI4O9MT.localdomain>
2021-11-21 18:37:12 +0530

    Update todos_controller to display DB content in array format

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,8 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   def index
-    render plain: "Hello, this is /todos!"
+    #render plain: "Hello, this is /todos!"
+    render plain: Todo.all.to_a
   end
 end
+
