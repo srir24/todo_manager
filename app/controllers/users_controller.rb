2e913a8c2d1f1faca9d9a019f1a44d411ae74b30
srir24 <ravula.srikanh@gmail.com>
2021-11-22 09:40:06 +0530

    Commented extra render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 class UsersController < ApplicationController#::Base
   skip_before_action :verify_authenticity_token
   def index
-    render plain: "Hello, this is users!"
+    #render plain: "Hello, this is users!"
     render plain: Users.all.to_a
     #render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
   end
