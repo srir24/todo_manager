b0d3a122ab7d0a3867edf31ec369975c7e9d2b28
srir24 <ravula.srikanh@gmail.com>
2021-11-22 11:33:36 +0530

    Added show, create, login features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
-# todos_controller.rb
-class UsersController < ApplicationController#::Base
+class UsersController<ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     #render plain: "Hello, this is users!"
     #render plain: Users.all.to_a
     render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
   end
-end
+  def show
+    id = params[:id].to_i
+    #render plain: "The id enter was #{id}"
+    user= Users.find(id)
+    render plain: user.to_pleasant
+  end
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    #render plain: "You are in create user with  name: #{name} and email : #{email}"
+    user=Users.create!(name: name, email: email, password:password)
+    render_txt="User created with Id #{user.id}"
+    render plain: render_txt
+  end
 
+  def login
+    email = params[:email]
+    password = params[:password]
+    user= Users.find_by(email: email, password: password)
+    render_txt = user ? "true" : "false"
+    render plain: render_txt
+
+  end
+end
