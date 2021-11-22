30c34f2df75fd3e76440d51c7beb66659fb319f8
srir24 <ravula.srikanh@gmail.com>
2021-11-22 09:42:32 +0530

    Updated index function to list the name and mail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController#::Base
   skip_before_action :verify_authenticity_token
   def index
     #render plain: "Hello, this is users!"
-    render plain: Users.all.to_a
-    #render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
+    #render plain: Users.all.to_a
+    render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
   end
 end
 
