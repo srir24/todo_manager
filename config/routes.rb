bf03a9398352b6a2e2d433d75dbbd624b800445d
root <root@DESKTOP-CI4O9MT.localdomain>
2021-11-22 07:14:29 +0530

    Added user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #get "todos", to: "todos#index"
   #post "todos/create", to: "todos#create"
   #get "todos/:id", to: "todos#show"
-  resources :todos
+  #resources :todos
+  get "users", to: "users#index"
+  #get "users/login", to: "users#show"
 end
