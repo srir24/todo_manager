bc1ff2bc9db30fe0e71aab0d27cd8a7ce12d0de6
srir24 <ravula.srikanh@gmail.com>
2021-11-25 00:16:07 +0530

    Removed unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get "todos", to: "todos#index"
-  #post "todos/create", to: "todos#create"
-  #get "todos/:id", to: "todos#show"
-  #resources :todos
-  #get "users", to: "users#index"
-  #get "users/login", to: "users#show"
-  #resources :users
   resources :todos, :users
   post "users/login", to: "users#login"
 end
