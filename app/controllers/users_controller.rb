8d8a18b9daa6ed8a421853f067279f15f8930548
srir24 <ravula.srikanh@gmail.com>
2021-11-25 00:17:46 +0530

    Removed unnecessary comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,10 @@
 class UsersController<ApplicationController
   skip_before_action :verify_authenticity_token
   def index
-    #render plain: "Hello, this is users!"
-    #render plain: Users.all.to_a
     render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
   end
   def show
     id = params[:id].to_i
-    #render plain: "The id enter was #{id}"
     user= Users.find(id)
     render plain: user.to_pleasant_user
   end
@@ -15,7 +12,6 @@ class UsersController<ApplicationController
     name = params[:name]
     email = params[:email]
     password = params[:password]
-    #render plain: "You are in create user with  name: #{name} and email : #{email}"
     user=Users.create!(name: name, email: email, password:password)
     render_txt="User created with Id #{user.id}"
     render plain: render_txt
