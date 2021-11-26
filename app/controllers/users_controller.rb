1b1f1dbf46fda0dc54ff6b979e38398277ab0b2f
srir24 <ravula.srikanh@gmail.com>
2021-11-26 10:52:47 +0530

    Removed unessary function and update class name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,15 @@
 class UsersController<ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Users.order(:name).map {|user| user.to_pleasant_user}.join("\n")
+    render plain: User.order(:name).map {|user| user.to_pleasant_user}.join("\n")
   end
-  def show
-    id = params[:id].to_i
-    user= Users.find(id)
-    render plain: user.to_pleasant_user
-  end
+
   def create
     name = params[:name]
     email = params[:email]
     password = params[:password]
-    user=Users.create!(name: name, email: email, password:password)
+    user=User.create!(name: name, email: email, password:password)
     render_txt="User created with Id #{user.id}"
     render plain: render_txt
   end
@@ -20,9 +17,8 @@ class UsersController<ApplicationController
   def login
     email = params[:email]
     password = params[:password]
-    user= Users.find_by(email: email, password: password)
+    user= User.find_by(email: email, password: password)
     render_txt = user ? "true" : "false"
     render plain: render_txt
-
   end
 end
